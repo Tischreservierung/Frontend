276a0962e1a8051c534a2345d5a237c2624ea6f7
Basti <sebastian2005witzeneder@gmail.com>
2023-04-17 11:39:58 +0200

    Dropdown with categories

--- src/app/components/restaurant/restaurant-filter/restaurant-filter.component.ts
+++ src/app/components/restaurant/restaurant-filter/restaurant-filter.component.ts
@@ -1,17 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { DeviceDetectorService, DeviceInfo } from 'ngx-device-detector';
+import { Category } from 'src/app/model/category';
+import { CategoryService } from 'src/app/service/category/category.service';
 
 @Component({
   selector: 'app-restaurant-filter',
   templateUrl: './restaurant-filter.component.html',
   styleUrls: ['./restaurant-filter.component.scss']
 })
-export class RestaurantFilterComponent {
+export class RestaurantFilterComponent implements OnInit{
   deviceInfo: DeviceInfo;
   showMobile: boolean = false;
+  categories: Category[] = [];
+  categoryControl = new FormControl<Category[] | null>(null);
 
-  constructor(private deviceService: DeviceDetectorService) {
+
+
+  constructor(private deviceService: DeviceDetectorService, private catService: CategoryService) {
     this.deviceInfo = this.deviceService.getDeviceInfo();
+  }
+  ngOnInit(): void {
+    this.catService.getCategories().subscribe({
+      next: data => { this.categories = data }
+    });
     this.epicFunction();
   }
 
